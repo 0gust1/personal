841ce9a73ea3935db350242a7a110b170345515a
Augustin Chassine <augustin.chassine@mabsilico.com>
2023-07-01 14:58:31 +0200

    feat: postcss nesting config

---
+++ postcss.config.js
@@ -0,0 +1,10 @@
+// ref: https://tailwindcss.com/docs/using-with-preprocessors
+export default {
+  plugins: {
+    //Some plugins, like tailwindcss/nesting, need to run before Tailwind,
+    'tailwindcss/nesting': {},
+    tailwindcss: {},
+    //But others, like autoprefixer, need to run after,
+    autoprefixer: {}
+  }
+};
