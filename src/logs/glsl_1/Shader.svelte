7cc645549f46f8bc13d9ee8fae55dd0dd2ff748c
Augustin Chassine <augustin.chassine@mabsilico.com>
2023-07-03 18:54:35 +0200

    feat: transform the Shader component into a small shader editor

---
+++ src/logs/glsl_1/Shader.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import WebGlCanvas from '$lib/components/WebGLCanvas.svelte';
+  export let fragShader: string;
+
+  let shaderCode = fragShader;
+</script>
+
+{#key shaderCode}
+  <WebGlCanvas fragShader={shaderCode} />
+{/key}
+
+<details>
+  <summary>GLSL code (editable)</summary>
+  <textarea bind:value={shaderCode} class="w-full h-96 p-4 font-mono text-xs" />
+</details>
