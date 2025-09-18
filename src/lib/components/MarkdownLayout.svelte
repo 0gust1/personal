4b075d35b98d854c9cc8f9c67847829d360600ae
Augustin Chassine <augustin.chassine@mabsilico.com>
2025-09-18 19:05:42 +0200

    feat: markdown layout file (could be useful later)

---
+++ src/lib/components/MarkdownLayout.svelte
@@ -0,0 +1,12 @@
+
+<script lang="ts">
+const { children } = $props();
+</script>
+
+{#if children}
+<div class="markdown-layout">
+	{@render children()}
+</div>
+{:else}
+	<p>fallback content</p>
+{/if}
